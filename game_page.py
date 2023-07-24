17e273942af43df72d6220ed676be72a181fd714
Ahsen <bucakahsen@gmail.com>
2023-07-24 11:36:29 +0200

    adding methods in game_page

--- game_page.py
+++ game_page.py
@@ -8,8 +8,15 @@ class GameWindow(QtWidgets.QMainWindow):
         self.object=object
         self.game=Game(self.object)
         self.game.starting_time()
+        self.true_button.setEnable(False)
+        self.false_button.setEnabled(False)
         super(GameWindow,self).__init__()
         uic.loadUi('ui/play_ui.ui',self)
+        self.back_button.clicked.connect(self.menu_show)
+        self.true_button.clicked.connect(self.true_button_function)
+        self.false_button.clicked.connect(self.false_button_fuction)
+        self.exit_button.clicked.connect(self.exit)
+        self.timer_
         self.show()
 
     def menu_show(self):
@@ -22,8 +29,50 @@ class GameWindow(QtWidgets.QMainWindow):
     def starting(self):
         self.word_nl,self.word_en,self.true_num=self.game.show_words()
         self.totalTry_num=self.game.attempts_number
-        self.language_label.setText("NEDERLANDS")
-        self.word_label.setText(self.word_nl)
-        self.true_label.setText(self.true_num)
-        self.try_label.setText(str(self.totalTry_num))
+        self.languages.setText("NEDERLANDS")
+        self.cards.setStyleSheet("border-color: rgb(170, 85, 0);\nbackground-color:rgb(5, 119, 161);\nborder-style: solid;\nborder-width: 3px;\nborder-radius:50px;")
+        self.true_button.setEnable(False)
+        self.false_button.setEnabled(False)
+        self.words_level.setProperty('Value',self.true_num)
+        self.words.setText(self.word_nl)
+        self.true_score.setText(self.true_num)
+        self.total_try.setText(str(self.totalTry_num))
         self.level_label.setText("LEVEL"+str(self.object.level))
+
+    def en_word(self,en):
+        self.en=en
+        self.languages.setText("ENGLISH")
+        self.cards.setStyleSheet("border-color: rgb(170, 85, 0);\nbackground-color:rgb(255, 255, 220);\nborder-style: solid;\nborder-width: 3px;\nborder-radius:50px;")
+        self.words.setText(self.word_en)
+        
+    def update(self):
+        self.timer_label.setText(str(self.count))
+        if self.count==0:
+            self.true_button.setEnable(True)
+            self.false_button.setEnabled(True)
+            self.timer.stop()
+            self.en_word(self.word_en)
+        self.count -=1
+
+
+    def timer_(self):
+        self.timer=QTimer(self)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(1000)
+        self.count=3
+        self.starting()
+    
+    def true_button_function(self):
+        self.game.true()
+        self.game.total_attempt_number()
+        self.timer_()
+        
+    def false_button_fuction(self):
+        self.game.false()
+        self.game.total_attempt_number()
+        self.timer_()
+
+    def exit(self):
+        self.passedTime=int(self.game.totalTime())
+        self.object.registerUserStat(self.object.level, self.passedTime)
+        self.close()
